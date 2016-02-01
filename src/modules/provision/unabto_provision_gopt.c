5a6c06c8d137b77334486e24424e13267a24c688
Martin Rodalgaard <mrodalgaard@gmail.com>
2016-02-01 10:30:46 +0100

    Correct provision help text

--- src/modules/provision/unabto_provision_gopt.c
+++ src/modules/provision/unabto_provision_gopt.c
@@ -60,11 +60,11 @@ void unabto_provision_gopt_help(const char* progname) {
     printf("    --%s=<key>: The provisioning service api key, matching the hostname pattern used\n", UNABTO_PROVISION_GOPT_NAME_API_KEY);
     printf("    --%s=<id>: The input id for the provisioning server (mac address or nabto id)\n", UNABTO_PROVISION_GOPT_NAME_ID_INPUT);
     printf("    --%s=<token>: Authorization token for the provisioning service\n", UNABTO_PROVISION_GOPT_NAME_USER_TOKEN);
-    printf("    --%s=<token>: File to store id and key\n", UNABTO_PROVISION_GOPT_NAME_FILE);
+    printf("    --%s=<file_path>: File to store id and key\n", UNABTO_PROVISION_GOPT_NAME_FILE);
     printf("Provisoning examples:\n");
     printf("    %s -P --%s=customer-b.provision.nabto.com --%s=0e:a1:c3:ed:eb:3f   # service issues id and key for mac\n", progname, UNABTO_PROVISION_GOPT_NAME_HOST, UNABTO_PROVISION_GOPT_NAME_ID_INPUT);
     printf("    %s -P --%s=customer-c.provision.nabto.com   # service issues id and key\n", progname, UNABTO_PROVISION_GOPT_NAME_HOST);
-    printf("    %s -P --%s=customer-d.provision.nabto.com --%s=e5a4-0c1e-99ed-f01a  # service issues id and key, checks one-time token\n", progname, UNABTO_PROVISION_GOPT_NAME_HOST, UNABTO_PROVISION_GOPT_NAME_ID_INPUT);
+    printf("    %s -P --%s=customer-d.provision.nabto.com --%s=E5A4-0C1E-99ED-F01A   # service issues id and key, checks one-time token\n", progname, UNABTO_PROVISION_GOPT_NAME_HOST, UNABTO_PROVISION_GOPT_NAME_USER_TOKEN);
     printf("    %s -P   # use results from earlier provisioning \n", progname);
 }
 
@@ -89,7 +89,7 @@ bool unabto_provision_gopt_apply(nabto_main_setup* nms, const char* progname, vo
         NABTO_LOG_ERROR(("Provision host not specified on cmd line or in source"));
         return false;
     }
-    
+
     if (!gopt_arg(options, UNABTO_PROVISION_GOPT_API_KEY, (const char**)(&(context.api_key_))) && !set_default_prov_apikey(&context)) {
         NABTO_LOG_ERROR(("Provision API key not specified on cmd line or in source"));
         return false;
