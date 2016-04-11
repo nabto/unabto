0cc57bb02ab12e04e2a88486785b8c608ff5571a
christiansiegel <cs@nabto.com>
2016-04-11 14:07:35 +0200

    use 4 spaces instead of tab

--- src/platforms/freertos_net/unabto_environment_freertos_net.c
+++ src/platforms/freertos_net/unabto_environment_freertos_net.c
@@ -22,7 +22,7 @@
 
 bool nabto_init_platform( void )
 {
-	return true;
+    return true;
 }
 /******************************************************************************/
 
@@ -38,13 +38,13 @@ static nabto_socket_t activeSockets[MAX_SOCKETS];
 
 nabto_socket_t *getActiveSockets(uint16_t *nS)
 {
-	nabto_socket_t * aS = NULL;
-	if (0 < nSockets)
-	{
-		*nS = nSockets;
-		aS = activeSockets;
-	}
-	return aS;
+    nabto_socket_t * aS = NULL;
+    if (0 < nSockets)
+    {
+        *nS = nSockets;
+        aS = activeSockets;
+    }
+    return aS;
 }
 
 bool nabto_init_socket( uint32_t localAddr, uint16_t* localPort, nabto_socket_t* socketDescriptor )
@@ -53,53 +53,53 @@ int to = 0;
 struct freertos_sockaddr xAddress, *pxAddressToUse;
 bool bReturn = true;
 
-	if (MAX_SOCKETS <= nSockets)
-	{
-		bReturn = false;
-	}
-	else
-	{
-		*socketDescriptor = FreeRTOS_socket( FREERTOS_AF_INET, FREERTOS_SOCK_DGRAM, FREERTOS_IPPROTO_UDP );
+    if (MAX_SOCKETS <= nSockets)
+    {
+        bReturn = false;
+    }
+    else
+    {
+        *socketDescriptor = FreeRTOS_socket( FREERTOS_AF_INET, FREERTOS_SOCK_DGRAM, FREERTOS_IPPROTO_UDP );
 
-		if( NULL == *socketDescriptor )
-		{
-			bReturn = false;
-		}
-		else
-		{
-			memset( &xAddress, 0, sizeof( xAddress ) );
-			xAddress.sin_addr = FreeRTOS_htonl( localAddr );
-			xAddress.sin_port = FreeRTOS_htons( *localPort );
-
-
-				pxAddressToUse = &xAddress;
+        if( NULL == *socketDescriptor )
+        {
+            bReturn = false;
+        }
+        else
+        {
+            memset( &xAddress, 0, sizeof( xAddress ) );
+            xAddress.sin_addr = FreeRTOS_htonl( localAddr );
+            xAddress.sin_port = FreeRTOS_htons( *localPort );
 
 
-			if( 0 > FreeRTOS_bind(*socketDescriptor, pxAddressToUse, sizeof( xAddress ) ) )
-			{
-				FreeRTOS_closesocket( *socketDescriptor );
-				bReturn = false;
-			}
-			else
-			{
-				/* Set receive time out to 0.  Timeouts are performed using a
-				select() call. */
-				FreeRTOS_setsockopt( *socketDescriptor, 0, FREERTOS_SO_RCVTIMEO, &to, sizeof( to ) );
-				activeSockets[nSockets++] = *socketDescriptor;
-			}
+            pxAddressToUse = &xAddress;
 
-			*localPort = FreeRTOS_htons( xAddress.sin_port );
-		}
-	}
-	return bReturn;
+
+            if( 0 > FreeRTOS_bind(*socketDescriptor, pxAddressToUse, sizeof( xAddress ) ) )
+            {
+                FreeRTOS_closesocket( *socketDescriptor );
+                bReturn = false;
+            }
+            else
+            {
+                /* Set receive time out to 0.  Timeouts are performed using a
+                select() call. */
+                FreeRTOS_setsockopt( *socketDescriptor, 0, FREERTOS_SO_RCVTIMEO, &to, sizeof( to ) );
+                activeSockets[nSockets++] = *socketDescriptor;
+            }
+
+            *localPort = FreeRTOS_htons( xAddress.sin_port );
+        }
+    }
+    return bReturn;
 }
 /******************************************************************************/
 
-ssize_t nabto_read(nabto_socket_t socket,
-                   uint8_t*       buf,
-                   size_t         len,
-                   uint32_t*      addr,
-                   uint16_t*      port)
+ssize_t nabto_read( nabto_socket_t socket,
+                    uint8_t*       buf,
+                    size_t         len,
+                    uint32_t*      addr,
+                    uint16_t*      port )
 {
     int res;
     struct freertos_sockaddr xAddress;
@@ -110,27 +110,27 @@ ssize_t nabto_read(nabto_socket_t socket,
 
     if( res > 0 )
     {
-    	*addr = FreeRTOS_htonl( xAddress.sin_addr );
-    	*port = FreeRTOS_htons( xAddress.sin_port ); 	
+        *addr = FreeRTOS_htonl( xAddress.sin_addr );
+        *port = FreeRTOS_htons( xAddress.sin_port );
     }
     else
-	{
-    	res = 0;
-	}
+    {
+        res = 0;
+    }
 
     return res;
 } 
 /******************************************************************************/
 
 ssize_t nabto_write( nabto_socket_t socket,
-                 const uint8_t* buf,
-                 size_t         len,
-                 uint32_t       addr,
-                 uint16_t       port)
+                     const uint8_t* buf,
+                     size_t         len,
+                     uint32_t       addr,
+                     uint16_t       port )
 {
     int res;
     struct freertos_sockaddr xAddress;
-	
+
     memset( &xAddress, 0, sizeof( xAddress ) );
     xAddress.sin_addr = FreeRTOS_htonl( addr );
     xAddress.sin_port = FreeRTOS_htons( port );
@@ -143,15 +143,15 @@ void nabto_close_socket( nabto_socket_t* socketDescriptor )
 {
     if (socketDescriptor) 
     {
-    	FreeRTOS_closesocket(*socketDescriptor);
-    	*socketDescriptor = NULL;
+        FreeRTOS_closesocket(*socketDescriptor);
+        *socketDescriptor = NULL;
     }
 }
 /******************************************************************************/
 
 void nabto_dns_resolve(const char* id) 
 {
-	( void ) id;
+    ( void ) id;
 }
 /******************************************************************************/
 
@@ -160,23 +160,23 @@ nabto_dns_status_t nabto_dns_is_resolved( const char *id, uint32_t* v4addr )
     uint32_t addr;
     nabto_dns_status_t status;
 
-   	addr = FreeRTOS_gethostbyname( id );
+    addr = FreeRTOS_gethostbyname( id );
 
     if( 0 == addr )
-	{
-      	status = NABTO_DNS_ERROR;
-	}
-	else
-	{
-		*v4addr = FreeRTOS_htonl(addr);
-		status = NABTO_DNS_OK;
-	}
+    {
+        status = NABTO_DNS_ERROR;
+    }
+    else
+    {
+        *v4addr = FreeRTOS_htonl(addr);
+        status = NABTO_DNS_OK;
+    }
 
     return status;
 }
 /******************************************************************************/
 
-void setTimeFromGSP(uint32_t stamp)
+void setTimeFromGSP( uint32_t stamp )
 {
-	NABTO_NOT_USED(stamp);
+    NABTO_NOT_USED(stamp);
 }
