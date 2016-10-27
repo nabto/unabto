a3c12083ec8e7922fe9402c1dde541ef0f96bb58
Michael Madsen <mkm@nabto.com>
2016-10-27 18:15:16 +0200

    uart tunnel

---
+++ src/modules/uart_tunnel/tunnel.h
@@ -0,0 +1,63 @@
+#ifndef _TUNNEL_H_
+#define _TUNNEL_H_
+
+#include <unabto_platform_types.h>
+#include <unabto/unabto_stream.h>
+
+typedef enum {
+    TS_IDLE,
+    TS_READ_COMMAND,
+    TS_PARSE_COMMAND,
+    TS_OPEN_UART,
+    TS_OPENING_SOCKET,
+    TS_FORWARD,
+    TS_CLOSING
+} tunnelStates;
+
+typedef enum {
+    FS_READ,
+    FS_WRITE,
+    FS_CLOSING
+} forwardState;
+
+typedef enum {
+    TUNNEL_EVENT_SOURCE_UART_WRITE,
+    TUNNEL_EVENT_SOURCE_UART_READ,
+    TUNNEL_EVENT_SOURCE_UNABTO
+} tunnel_event_source;
+
+struct tunnel_static_memory;
+
+typedef struct tunnel {
+#if NABTO_ENABLE_EPOLL
+    int epollEventType;
+#endif
+    unabto_stream* stream;
+    tunnelStates state;
+    int commandLength;
+    int port;
+    int fd;
+    forwardState uartReadState;
+    forwardState unabtoReadState;
+    int uartReadBufferSize;
+    int uartReadBufferSent;
+    int tunnelId;
+    struct tunnel_static_memory* staticMemory;
+} tunnel;
+
+extern NABTO_THREAD_LOCAL_STORAGE tunnel* tunnels;
+
+void tunnel_set_default_host(const char* host);
+void tunnel_set_default_port(int port);
+
+const char* tunnel_get_default_host();
+int tunnel_get_default_port();
+
+bool init_tunnel_module();
+void deinit_tunnel_module();
+
+void reset_tunnel_struct(tunnel* t);
+
+void tunnel_event(tunnel* state, tunnel_event_source event_source);
+
+#endif // _TUNNEL_H_
