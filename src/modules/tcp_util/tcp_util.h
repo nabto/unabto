afec7e2581f3bd579eb8ce1f1c2000eefb0fca3a
fusionfoxy <thom2287@gmail.com>
2017-10-11 14:03:18 +0200

    NABTO-1551: interface update

--- src/modules/tcp_util/tcp_util.h
+++ src/modules/tcp_util/tcp_util.h
@@ -1,17 +1,20 @@
+#ifdef WIN32
+typedef SOCKET unabtoSocket;
+#else
+typedef int unabtoSocket;
+#endif
 
+size_t write(unabtoSocket sock, const void* buf, size_t len);
 
+int uanbto_close_socket(unabtoSocket sockfd)
 
+bool unabto_tcp_util_would_block();
 
-write(s,d,l)
-closesocket(s)
+bool unabto_tcp_util_in_progress();
 
-bool would_block();
+bool unabto_tcp_util_init_local(unabtoSocket sockfd);
 
-bool in_progress();
-
-bool unabto_tcp_util_init_local(tcp_fallback_socket sockfd);
-
-bool unabto_tcp_util_init_remote(tcp_fallback_socket sockfd);
+bool unabto_tcp_util_init_remote(unabtoSocket sockfd);
 
 bool unabto_tcp_util_handle_connect(nabto_connect* con);
 
