337b15b9b4129fb0dea4231b5b6065f200bcadd4
Ulrik Gammelby <ug@nabto.com>
2018-04-09 16:37:48 +0200

    use correct indentation

--- apps/aclcli/src/unabto_acl.c
+++ apps/aclcli/src/unabto_acl.c
@@ -1,13 +1,13 @@
 /************************************************************************************************
-* 
-* Filename   : unabto_acl.c 
-* Description: To add, remove & list ACL file contents
-* Usage      : unabto_acl <[add]/[remove]/[list]> <ACL filename>
-* Note       : During addition of new users,  by default allow local access and remote access 
-*              permissions will be granted.
-*              In case, the new user is the first user, then admin access will be granted.
-* 
-************************************************************************************************/
+ * 
+ * Filename   : unabto_acl.c 
+ * Description: To add, remove & list ACL file contents
+ * Usage      : unabto_acl <[add]/[remove]/[list]> <ACL filename>
+ * Note       : During addition of new users,  by default allow local access and remote access 
+ *              permissions will be granted.
+ *              In case, the new user is the first user, then admin access will be granted.
+ * 
+ ************************************************************************************************/
 
 #include <stdio.h>
 #include <stdint.h>
@@ -77,8 +77,8 @@ int main(int argc, char* argv[]) {
     // Test file existence
     FILE* aclFile = fopen(config.aclfilename, "rb+");
     if (aclFile == NULL) {
-      NABTO_LOG_ERROR(("Could not load aclfile %s", config.aclfilename));
-      return FP_ACL_DB_LOAD_FAILED;
+        NABTO_LOG_ERROR(("Could not load aclfile %s", config.aclfilename));
+        return FP_ACL_DB_LOAD_FAILED;
     }
     fclose(aclFile);
 
@@ -88,28 +88,28 @@ int main(int argc, char* argv[]) {
     struct fp_mem_persistence p;
 
     if (fp_acl_file_init(config.aclfilename, "tmp.bin", &p) != FP_ACL_DB_OK) {
-      NABTO_LOG_ERROR(("cannot init: fp_acl_file_init:%s", config.aclfilename));
-      return FP_ACL_DB_FAILED;
+        NABTO_LOG_ERROR(("cannot init: fp_acl_file_init:%s", config.aclfilename));
+        return FP_ACL_DB_FAILED;
     }
         
     if (fp_mem_init(&db, &defaultSettings, &p) != FP_ACL_DB_OK) {
-      NABTO_LOG_ERROR(("cannot init: fp_mem_init"));
-      return FP_ACL_DB_FAILED;
+        NABTO_LOG_ERROR(("cannot init: fp_mem_init"));
+        return FP_ACL_DB_FAILED;
     }
 
     if (!strcmp(config.action, "add")) {
-      if (fp_acl_file_add_file(&config, &db) != FP_ACL_DB_OK)
-	    printf("Add Failed\n");
+        if (fp_acl_file_add_file(&config, &db) != FP_ACL_DB_OK)
+            printf("Add Failed\n");
     }
     
     if (!strcmp(config.action, "list")) {
-      if (fp_acl_file_list_file(&config, &db) != FP_ACL_DB_OK)
-	printf("List Failed\n");
+        if (fp_acl_file_list_file(&config, &db) != FP_ACL_DB_OK)
+            printf("List Failed\n");
     }
 
     if (!strcmp(config.action, "remove")) {
-      if (fp_acl_file_remove_file(&config, &db) != FP_ACL_DB_OK)
-	    printf("Remove Failed\n");
+        if (fp_acl_file_remove_file(&config, &db) != FP_ACL_DB_OK)
+            printf("Remove Failed\n");
     }
 
     exit(0);
@@ -119,103 +119,103 @@ int main(int argc, char* argv[]) {
 fp_acl_db_status fp_acl_file_list_file(struct configuration* config, struct fp_acl_db* db)
 {
 
-  void* iterator;
-  int numUsers = 0;
+    void* iterator;
+    int numUsers = 0;
 
-  struct fp_acl_settings acl_settings;
+    struct fp_acl_settings acl_settings;
   
-  if(db->load_settings(&acl_settings) != FP_ACL_DB_OK) {
-    NABTO_LOG_ERROR(("Could not load acl"));
-    return FP_ACL_DB_LOAD_FAILED;
-  }
-
-  printf("System Permissions       : %04x:%04x \n", splithex(acl_settings.systemPermissions));
-  printf("Default User Permissions : %04x:%04x \n", splithex(acl_settings.defaultUserPermissions));
-  printf("First User Permissions   : %04x:%04x \n", splithex(acl_settings.firstUserPermissions));
-
-
-  // Count users
-  iterator = db->first();
-  // move pointer forward
-  while (iterator != NULL) {
-    iterator = db->next(iterator);
-    numUsers++;
-  } 
-
-  printf("Number of users = %d\n", numUsers);
-  
-  numUsers = 0;
-
-  // Run through db and list users
-  iterator = db->first();
-  // move pointer forward
-  while (iterator != NULL) {
-
-    struct fp_acl_user user;
-    numUsers++;
-
-    if(db->load(iterator, &user) != FP_ACL_DB_OK) {
-      printf("Could not load user %d", numUsers);
-    } else {
-      int j;
-      for (j=0; j<FP_ACL_FP_LENGTH;j++) {
-	if (j) printf(":");
-	printf("%02x", user.fp[j]);
-      }
-      
-      printf("  %04x:%04x  %s\n", splithex(user.permissions) , user.name);
+    if(db->load_settings(&acl_settings) != FP_ACL_DB_OK) {
+        NABTO_LOG_ERROR(("Could not load acl"));
+        return FP_ACL_DB_LOAD_FAILED;
     }
-    iterator=db->next(iterator);
 
-  }
+    printf("System Permissions       : %04x:%04x \n", splithex(acl_settings.systemPermissions));
+    printf("Default User Permissions : %04x:%04x \n", splithex(acl_settings.defaultUserPermissions));
+    printf("First User Permissions   : %04x:%04x \n", splithex(acl_settings.firstUserPermissions));
+
+
+    // Count users
+    iterator = db->first();
+    // move pointer forward
+    while (iterator != NULL) {
+        iterator = db->next(iterator);
+        numUsers++;
+    } 
+
+    printf("Number of users = %d\n", numUsers);
   
-  return FP_ACL_DB_OK;
+    numUsers = 0;
+
+    // Run through db and list users
+    iterator = db->first();
+    // move pointer forward
+    while (iterator != NULL) {
+
+        struct fp_acl_user user;
+        numUsers++;
+
+        if(db->load(iterator, &user) != FP_ACL_DB_OK) {
+            printf("Could not load user %d", numUsers);
+        } else {
+            int j;
+            for (j=0; j<FP_ACL_FP_LENGTH;j++) {
+                if (j) printf(":");
+                printf("%02x", user.fp[j]);
+            }
+      
+            printf("  %04x:%04x  %s\n", splithex(user.permissions) , user.name);
+        }
+        iterator=db->next(iterator);
+
+    }
+  
+    return FP_ACL_DB_OK;
 }
 
 // removing an entry from ACL file
 fp_acl_db_status fp_acl_file_remove_file(struct configuration* config, struct fp_acl_db* db)
 {
   
-  void* it;
-  struct fp_acl_user user;
-  struct fp_acl_settings aclSettings;
+    void* it;
+    struct fp_acl_user user;
+    struct fp_acl_settings aclSettings;
 
-  // Load acl settings from file
-  if(db->load_settings(&aclSettings) != FP_ACL_DB_OK) {
-    NABTO_LOG_ERROR(("Could not load aclsettings"));
-    return FP_ACL_DB_LOAD_FAILED;
-  }
-
-
-
-  // If first user - use firstUserPermissions otherwise use defaultUserPermission
-  if (db->first() == NULL) {
-    user.permissions = aclSettings.firstUserPermissions;
-  } else {
-    user.permissions = aclSettings.defaultUserPermissions;
-  }
-
-  
-  if (fp_get_fingerprint(config->fingerprint, user.fp) != 1) {
-    NABTO_LOG_ERROR(("Invalid Fingerprint\n"));
-    return FP_ACL_DB_LOAD_FAILED;
-  }
-
-
-  it = db->find(user.fp);
-  
-  if (it == 0 || db->load(it, &user) != FP_ACL_DB_OK) {
-    printf("No such fingerprint");
-  } else {
-    if (db->remove(it) == FP_ACL_DB_OK) {
-      printf("Remove user");
-    } else {
-      printf("Could not remove user");
+    // Load acl settings from file
+    if(db->load_settings(&aclSettings) != FP_ACL_DB_OK) {
+        NABTO_LOG_ERROR(("Could not load aclsettings"));
+        return FP_ACL_DB_LOAD_FAILED;
     }
-  }
+
+
+
+    // If first user - use firstUserPermissions otherwise use defaultUserPermission
+    if (db->first() == NULL) {
+        user.permissions = aclSettings.firstUserPermissions;
+    } else {
+        user.permissions = aclSettings.defaultUserPermissions;
+    }
 
   
-  return FP_ACL_DB_OK;
+    if (fp_get_fingerprint(config->fingerprint, user.fp) != 1) {
+        NABTO_LOG_ERROR(("Invalid Fingerprint\n"));
+        return FP_ACL_DB_LOAD_FAILED;
+    }
+
+
+    it = db->find(user.fp);
+  
+    if (it == 0 || db->load(it, &user) != FP_ACL_DB_OK) {
+        printf("No such fingerprint");
+    } else {
+        if (db->remove(it) == FP_ACL_DB_OK) {
+            printf("Remove user");
+        } else {
+            printf("Could not remove user");
+        }
+    }
+
+  
+    return FP_ACL_DB_OK;
 
 }
 
@@ -223,36 +223,36 @@ fp_acl_db_status fp_acl_file_remove_file(struct configuration* config, struct fp
 fp_acl_db_status fp_acl_file_add_file(struct configuration* config, struct fp_acl_db* db)
 {
   
-  fp_acl_db_status status;
+    fp_acl_db_status status;
 
-  struct fp_acl_user user;
-  struct fp_acl_settings aclSettings;
+    struct fp_acl_user user;
+    struct fp_acl_settings aclSettings;
 
-  // Load acl settings from file
-  if(db->load_settings(&aclSettings) != FP_ACL_DB_OK) {
-    NABTO_LOG_ERROR(("Could not load aclsettings"));
-    return FP_ACL_DB_LOAD_FAILED;
-  }
+    // Load acl settings from file
+    if(db->load_settings(&aclSettings) != FP_ACL_DB_OK) {
+        NABTO_LOG_ERROR(("Could not load aclsettings"));
+        return FP_ACL_DB_LOAD_FAILED;
+    }
 
-  // If first user - use firstUserPermissions otherwise use defaultUserPermission
-  if (db->first() == NULL) {
-    user.permissions = aclSettings.firstUserPermissions;
-  } else {
-    user.permissions = aclSettings.defaultUserPermissions;
-  }
+    // If first user - use firstUserPermissions otherwise use defaultUserPermission
+    if (db->first() == NULL) {
+        user.permissions = aclSettings.firstUserPermissions;
+    } else {
+        user.permissions = aclSettings.defaultUserPermissions;
+    }
 
   
-  if (fp_get_fingerprint(config->fingerprint, user.fp) != 1) {
-    NABTO_LOG_ERROR(("Invalid Fingerprint\n"));
-    return FP_ACL_DB_LOAD_FAILED;
-  }
+    if (fp_get_fingerprint(config->fingerprint, user.fp) != 1) {
+        NABTO_LOG_ERROR(("Invalid Fingerprint\n"));
+        return FP_ACL_DB_LOAD_FAILED;
+    }
 
 
-  memcpy(user.name, config->user, FP_ACL_USERNAME_MAX_LENGTH);
+    memcpy(user.name, config->user, FP_ACL_USERNAME_MAX_LENGTH);
 
-  db->save(&user);
+    db->save(&user);
 
-  return FP_ACL_DB_OK;
+    return FP_ACL_DB_OK;
 
 }
 
@@ -263,26 +263,26 @@ bool fp_get_fingerprint(const char *fpargv, fingerprint fpLocal)
     i=0;j=0; bool last;
 
     if(strlen(fpargv)!=FP_ACL_FP_LENGTH*3-1) 
-      return false;
+        return false;
 
     for (j=0; j<FP_ACL_FP_LENGTH;j++) {
-      last = (j == FP_ACL_FP_LENGTH-1);
+        last = (j == FP_ACL_FP_LENGTH-1);
 
-      if(strchr("0123456789abcdefABCDEF", fpargv[i]) == NULL ||
-	 strchr("0123456789abcdefABCDEF", fpargv[i+1]) == NULL) {
-	printf("invalid hexchar in finger print");
-	return false;
-      }
-      if(!last) {
-	if(fpargv[i+2] != ':') {
-	  printf("hexvalues must be separated by ':'  ");
-	  return false;
-	}
-	sscanf(fpargv+i, "%2hhx:", &fpLocal[j]);
-      } else {
-	sscanf(fpargv+i, "%2hhx", &fpLocal[j]);
-      }
-      i+=3;
+        if(strchr("0123456789abcdefABCDEF", fpargv[i]) == NULL ||
+           strchr("0123456789abcdefABCDEF", fpargv[i+1]) == NULL) {
+            printf("invalid hexchar in finger print");
+            return false;
+        }
+        if(!last) {
+            if(fpargv[i+2] != ':') {
+                printf("hexvalues must be separated by ':'  ");
+                return false;
+            }
+            sscanf(fpargv+i, "%2hhx:", &fpLocal[j]);
+        } else {
+            sscanf(fpargv+i, "%2hhx", &fpLocal[j]);
+        }
+        i+=3;
 
     }
 
@@ -315,7 +315,7 @@ bool parse_argv(int argc, char* argv[], struct configuration* config)
     if (argc <= 1) return false;
     config->action = strdup(argv[1]);
     if (strcmp(config->action, "list") && strcmp(config->action, "add") && strcmp(config->action, "remove")) {
-       	    return false;
+        return false;
     }
 
     if (!gopt_arg(options, 'F', &config->aclfilename)) {
