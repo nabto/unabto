4ef23dfb3720d9052aebca0285d093c71f54bc68
Michael <mkm@nabto.com>
2018-09-12 10:49:52 +0200

    40

--- src/unabto/unabto_message.c
+++ src/unabto/unabto_message.c
@@ -403,8 +403,8 @@ bool nabto_ep_is_equal(const nabto_endpoint* ep1, const nabto_endpoint* ep2)
 
 const char* nabto_ip_to_string(const struct nabto_ip_address* addr)
 {
-    static char output[20]; //0000:1111:2222:3333:4444:5555:6666:7777:8888\0"
-    memset(output, 0, 20);
+    static char output[40]; //0000:1111:2222:3333:4444:5555:6666:7777:8888\0"
+    memset(output, 0, 40);
     if (addr->type == NABTO_IP_NONE) {
         sprintf(output, "NABTO_IP_NONE");
     } else if (addr->type == NABTO_IP_ANY) {
