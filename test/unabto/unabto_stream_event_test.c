47f723e814d00c815c068de26f24a63778665040
Michael Madsen <mkm@nabto.com>
2018-11-14 14:40:36 +0100

    fixed tests

--- test/unabto/unabto_stream_event_test.c
+++ test/unabto/unabto_stream_event_test.c
@@ -60,46 +60,46 @@ void resetStream(struct nabto_stream_s* stream)
 
 bool test_state_machine(void) {
     bool ret = true;
-    struct nabto_stream_s stream;
+    struct nabto_stream_s* stream = &stream__[0];
 
-    resetStream(&stream);
+    resetStream(stream);
 
     struct nabto_win_info window;
     make_syn_window(&window);
 
-    stream.u.tcb.streamState = ST_IDLE;
+    stream->u.tcb.streamState = ST_IDLE;
     
-    ret &= test_state_machine_case(&stream, &window, ST_SYN_RCVD);
+    ret &= test_state_machine_case(stream, &window, ST_SYN_RCVD);
 
-    stream.u.tcb.streamState = ST_SYN_RCVD;
-    ret &= test_state_machine_case(&stream, &window, ST_SYN_RCVD);
+    stream->u.tcb.streamState = ST_SYN_RCVD;
+    ret &= test_state_machine_case(stream, &window, ST_SYN_RCVD);
 
     NABTO_LOG_INFO(("Errors in the following lines is expected:"));
 
     int i;
     for (i = ST_ESTABLISHED; i <= ST_CLOSED; i++) {
-        stream.u.tcb.streamState = i;
-        ret &= test_state_machine_case(&stream, &window, i);
+        stream->u.tcb.streamState = i;
+        ret &= test_state_machine_case(stream, &window, i);
     }
 
-    stream.u.tcb.streamState = ST_ESTABLISHED;
-    stream.u.tcb.recvNext = 33;
-    stream.u.tcb.xmitSeq = 42;
+    stream->u.tcb.streamState = ST_ESTABLISHED;
+    stream->u.tcb.recvNext = 33;
+    stream->u.tcb.xmitSeq = 42;
 
     // The fin has sequence number 33
     make_fin_ack_window(&window, 42, 33);
-    ret &= test_state_machine_case(&stream, &window, ST_CLOSE_WAIT);
+    ret &= test_state_machine_case(stream, &window, ST_CLOSE_WAIT);
     
-    stream.u.tcb.streamState = ST_LAST_ACK;
+    stream->u.tcb.streamState = ST_LAST_ACK;
     // Assume a fin packet has been sent with sequence number 42.
-    stream.u.tcb.finSequence = stream.u.tcb.xmitSeq;
-    stream.u.tcb.xmitFirst = stream.u.tcb.xmitSeq;
-    stream.u.tcb.xmitLastSent = stream.u.tcb.xmitFirst;
-    stream.u.tcb.xmitLastSent++;
-    stream.u.tcb.xmitSeq++;
+    stream->u.tcb.finSequence = stream->u.tcb.xmitSeq;
+    stream->u.tcb.xmitFirst = stream->u.tcb.xmitSeq;
+    stream->u.tcb.xmitLastSent = stream->u.tcb.xmitFirst;
+    stream->u.tcb.xmitLastSent++;
+    stream->u.tcb.xmitSeq++;
     
     make_ack_window(&window, 43, 34);
-    ret &= test_state_machine_case(&stream, &window, ST_CLOSED);    
+    ret &= test_state_machine_case(stream, &window, ST_CLOSED);    
 
     return ret;
 }
