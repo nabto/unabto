cf44e19d8cc991a342ba5c538d01ed3846eef1e8
Ulrik Gammelby <ug@nabto.com>
2019-12-20 15:37:34 +0100

    bump patch version

--- src/unabto_version.h
+++ src/unabto_version.h
@@ -31,7 +31,7 @@
  */
 #define UNABTO_VERSION_MAJOR            4
 #define UNABTO_VERSION_MINOR            4
-#define UNABTO_VERSION_PATCH            3
+#define UNABTO_VERSION_PATCH            4
 
 /**
  * Prerelease identifiers are used to distinguish real releases from
